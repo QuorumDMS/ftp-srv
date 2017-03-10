b7e17af99e8f1e080ddf9060d7c3116ff3f86497
Tyler Stewart <tylers490@gmail.com>
2017-03-10 13:42:45 -0700

    feat: allow STRU to take name suggesstion

--- src/commands/registration/stou.js
+++ src/commands/registration/stou.js
@@ -4,10 +4,16 @@ module.exports = {
   directive: 'STOU',
   handler: function (args) {
     if (!this.fs) return this.reply(550, 'File system not instantiated');
-    if (!this.fs.getUniqueName) return this.reply(402, 'Not supported by file system');
+    if (!this.fs.get || !this.fs.getUniqueName) return this.reply(402, 'Not supported by file system');
 
-    args.command._[1] = this.fs.getUniqueName();
-    return stor.call(this, args);
+    const fileName = args.command._[1];
+    return this.fs.get(fileName)
+    .catch(() => fileName)                // does not exist, name is unique
+    .then(() => this.fs.getUniqueName())  // exists, must create new unique name
+    .then(name => {
+      args.command._[1] = name;
+      return stor.call(this, args);
+    });
   },
   syntax: '{{cmd}}',
   description: 'Store file uniquely'
