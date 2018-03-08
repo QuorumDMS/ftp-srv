8566df451e5324470b3c44d7bad7253432909fe6
Tyler Stewart <tylers490@gmail.com>
2018-03-07 21:05:41 -0700

    refactor(logo): async/await logo generation

--- logo/generate.js
+++ logo/generate.js
@@ -1,23 +1,19 @@
 const puppeteer = require('puppeteer');
-const logoPath = `file://${process.cwd()}/logo/logo.html`;
 
-puppeteer.launch()
-.then(browser => {
-  return browser.newPage()
-  .then(page => {
-    return page.goto(logoPath)
-    .then(() => page);
-  })
-  .then(page => {
-    return page.setViewport({
-      width: 600,
-      height: 250,
-      deviceScaleFactor: 2
-    })
-    .then(() => page.screenshot({
-      path: 'logo.png',
-      omitBackground: true
-    }));
-  })
-  .then(() => browser.close());
-});
+(async function () {
+  const logoPath = `file://${process.cwd()}/logo/logo.html`;
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(logoPath);
+  await page.setViewport({
+    width: 600,
+    height: 250,
+    deviceScaleFactor: 2
+  });
+  await page.screenshot({
+    path: 'logo.png',
+    omitBackground: true
+  });
+  await browser.close();
+})();
