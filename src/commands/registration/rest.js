2f8b6184e3543dc6a95e4c9a41d83040f6e747f2
Jorin Vogel <hi@jorin.me>
2018-03-27 09:28:02 +0200

    fix(rest): small typo

--- src/commands/registration/rest.js
+++ src/commands/registration/rest.js
@@ -9,7 +9,7 @@ module.exports = {
     if (isNaN(byteCount) || byteCount < 0) return this.reply(501, 'Byte count must be 0 or greater');
 
     this.restByteCount = byteCount;
-    return this.reply(350, `Resarting next transfer at ${byteCount}`);
+    return this.reply(350, `Restarting next transfer at ${byteCount}`);
   },
   syntax: '{{cmd}} <byte-count>',
   description: 'Restart transfer from the specified point. Resets after any STORE or RETRIEVE'
