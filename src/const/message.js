fc8981021ce41c43e94413cbaf74fa579d6af609
Tyler Stewart <tylers490@gmail.com>
2018-03-28 20:33:04 -0600

    WIP: message constants

--- src/const/message.js
+++ src/const/message.js
@@ -1,3 +1,12 @@
 module.exports = {
-  GREETING: '220 Greetings'
+  SUPERFLUOUS_COMMAND: '202 Superfluous command',
+  GREETING: '220 Greetings',
+  AUTHENTICATED: '230 User authenticated successfully',
+  AWAITING_PASSWORD: '331 Username okay, awaiting password',
+  SYNTAX_ERROR_ARGS: '501 Syntax error in arguments',
+  UNSUPPORTED_COMMAND: '502 Command not supported',
+  BAD_COMMAND_SEQUENCE: '503 Bad sequence of commands',
+  USERNAME_SET_ALREADY: '530 Username already set',
+  COMMAND_REQUIRES_AUTHENTICATION: '530 Username already set',
+  AUTHENTICATED_FAILED: '530 Authentication failed',
 };
