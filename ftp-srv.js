91be338ebd965c703e606adef612a885ce439d53
Tyler Stewart <tylers490@gmail.com>
2018-07-22 18:47:23 -0600

    fix: expose errors as export

--- ftp-srv.js
+++ ftp-srv.js
@@ -1,6 +1,8 @@
 const FtpSrv = require('./src');
 const FileSystem = require('./src/fs');
+const errors = require('./src/errors');
 
 module.exports = FtpSrv;
 module.exports.FtpSrv = FtpSrv;
 module.exports.FileSystem = FileSystem;
+module.exports.ftpErrors = errors;
