e449e75219d918c400dec65b4b0759f60476abca
Tyler Stewart <tyler@autovance.com>
2020-08-17 12:48:21 -0600

    fix: disallow PORT connections to alternate hosts
    Ensure the data socket that the server connects to from the PORT command is the same IP as the current command socket.
    
    * fix: add error handling to additional connection commands

--- src/commands/registration/eprt.js
+++ src/commands/registration/eprt.js
@@ -8,14 +8,18 @@ const FAMILY = {
 
 module.exports = {
   directive: 'EPRT',
-  handler: function ({command} = {}) {
+  handler: function ({log, command} = {}) {
     const [, protocol, ip, port] = _.chain(command).get('arg', '').split('|').value();
     const family = FAMILY[protocol];
     if (!family) return this.reply(504, 'Unknown network protocol');
 
     this.connector = new ActiveConnector(this);
     return this.connector.setupConnection(ip, port, family)
-    .then(() => this.reply(200));
+    .then(() => this.reply(200))
+    .catch((err) => {
+      log.error(err);
+      return this.reply(err.code || 425, err.message);
+    });
   },
   syntax: '{{cmd}} |<protocol>|<address>|<port>|',
   description: 'Specifies an address and port to which the server should connect'
