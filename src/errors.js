28130d7dd9daa7570aed3c7f5454dee5de4be454
paulober <44974737+paulober@users.noreply.github.com>
2022-08-30 11:47:40 +0200

    Fix #338, Parse error message to super constructor

--- src/errors.js
+++ src/errors.js
@@ -1,37 +1,33 @@
 
 class GeneralError extends Error {
   constructor(message, code = 400) {
-    super();
+    super(message);
     this.code = code;
     this.name = 'GeneralError';
-    this.message = message;
   }
 }
 
 class SocketError extends Error {
   constructor(message, code = 500) {
-    super();
+    super(message);
     this.code = code;
     this.name = 'SocketError';
-    this.message = message;
   }
 }
 
 class FileSystemError extends Error {
   constructor(message, code = 400) {
-    super();
+    super(message);
     this.code = code;
     this.name = 'FileSystemError';
-    this.message = message;
   }
 }
 
 class ConnectorError extends Error {
   constructor(message, code = 400) {
-    super();
+    super(message);
     this.code = code;
     this.name = 'ConnectorError';
-    this.message = message;
   }
 }
 
