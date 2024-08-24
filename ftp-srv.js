e69c3537790976ef0eb2b4f7fea0d90ab1ccb3d7
Yonggang Luo <luoyonggang@gmail.com>
2024-08-24 14:56:30 +0800

    feat: getting ftp-srv.js match ftp-srv.d.ts
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>

--- ftp-srv.js
+++ ftp-srv.js
@@ -1,8 +1,14 @@
-const FtpSrv = require('./src');
+const FtpServer = require('./src');
+const FtpConnection = require('./src/connection');
 const FileSystem = require('./src/fs');
 const errors = require('./src/errors');
 
-module.exports = FtpSrv;
-module.exports.FtpSrv = FtpSrv;
+module.exports = FtpServer;
+module.exports.FtpSrv = FtpServer;
+module.exports.FtpServer = FtpServer;
 module.exports.FileSystem = FileSystem;
-module.exports.ftpErrors = errors;
+module.exports.GeneralError = errors.GeneralError;
+module.exports.SocketError = errors.SocketError;
+module.exports.FileSystemError = errors.FileSystemError;
+module.exports.ConnectorError = errors.ConnectorError;
+module.exports.FtpConnection = FtpConnection;
